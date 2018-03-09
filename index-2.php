d2504fe80241d924a341f01d638bf47afa7d745b
madhur1807 <madhurmittal1807@gmail.com>
2018-03-09 18:14:26 +0530

    ..

--- index-2.php
+++ index-2.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--meta responsive-->
     
     <!--START CSS--> 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="css/nicdark_style.css"> <!--style-->
     <link rel="stylesheet" href="css/nicdark_responsive.css"> <!--nicdark_responsive-->
 
